0e4abdbadad7e0535e2aa6f533b1b57ad859bb82
muralimandula <murali.mandula@msitprogram.net>
2018-08-29 00:06:24 +0530

    "check_style_score -> 4 of 5 passed. style score: 0/1 "

---
+++ cspp2-assignments/m2/Assignment-3/Solution.java
@@ -0,0 +1,40 @@
+/**
+*Assignment -3.
+* Finding a to power of b.
+*/
+
+import java.util.Scanner;
+
+/**
+*Main class - Solution Class.
+*/
+
+public final class Solution {
+    /**
+    *dummy.
+    */
+    private Solution() {
+    }
+    /**
+    *Do not modify this main function.
+    @param args Format need
+    */
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);      
+        int base = s.nextInt();
+        int exponent = s.nextInt();
+        int result = power(base, exponent);
+        System.out.println(result);
+	}
+	/**
+	*Need to write the power function and print the output.
+	*
+	*/
+	public static int power(final int a, final int b) {
+		if (b == 0){
+			return 1;
+		} else {
+			return a * power(a, b - 1);
+		}
+	}
+}
