9e35ef428f03dd7a7934c1743f90ce431ae3aa4f
muralimandula <murali.mandula@msitprogram.net>
2018-08-28 09:50:00 +0530

    HelloWorld_ Java begins

---
+++ cspp2-practice/m1/a3/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+Java begins.
+*/
+
+public final class HelloWorld {
+  /** Private constructor.
+  */
+  private HelloWorld() {
+    //abstraction
+  }
+
+  /**
+  First method in Java.
+  @param args of type String[]
+  * above tag is to store the command line
+  */
+
+  public static void main(final String[] args) {
+    System.out.println("Hello, World");
+  }
+}
