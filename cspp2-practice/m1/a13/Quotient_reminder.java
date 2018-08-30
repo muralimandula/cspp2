61c09538fe9121dc3542618cd8f33210fb0646d3
muralimandula <murali.mandula@msitprogram.net>
2018-08-30 20:13:02 +0530

    quotient reminder_missing

---
+++ cspp2-practice/m1/a13/Quotient_reminder.java
@@ -0,0 +1,28 @@
+/**
+  * 20186065.
+  * Java: QuotientReminder.
+  */
+public final class QuotientReminder {
+  /**
+  * Java: main class.
+  */
+   private QuotientReminder() {
+    /**
+    * Java: Empty constructor.
+    */
+   }
+
+   /**
+  * 20186065.
+  * Java: QuotientReminder.
+  @param args argument reader
+  */
+
+  public static void main(final String[] args) {
+    final int a = 10;
+    final int b = 2;
+    // int temp = 0;
+    System.out.println("Quotient of " + a + ", " + b + " is " + a / b);
+    System.out.println("Reminder of " + a + ", " + b + " is " + a % b);
+  }
+}
