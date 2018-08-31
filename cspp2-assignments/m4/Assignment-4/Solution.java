6d3d42fbead1742dd9e0997608d087e6ebde872b
muralimandula <murali.mandula@msitprogram.net>
2018-08-31 21:09:38 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-4/Solution.java
+++ cspp2-assignments/m4/Assignment-4/Solution.java
@@ -7,39 +7,42 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-public class Solution
-{
+public class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        // unused
+    }
 
-  
 
     /**
-	 * Do not modify the main function 
-	 * Main Method.
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args)
-	{
-		Scanner sc=new Scanner(System.in);
-		String s=sc.nextLine();
-		String reverse=reverseString(s);	
-		System.out.println(reverse);
-		
-	}
-    
+     * Do not modify the main function.
+     * Main Method.
+     * @param      args  The arguments.
+     */
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String reverse = reverseString(s);    
+        System.out.println(reverse); 
+    }
+
     /**
      * reversal function.
-     * @param      s     { parameter_description }
-     * @return     { description_of_the_return_value }
+     * @param      s     { parameter_description }.
+     * @return     { description_of_the_return_value }.
      */
 
-	public static String reverseString(String s) {
-		String str = "";
-		int length = s.length();
-		for (int i = 0; i < length; i++) {
-			str += s.charAt((length-1)-i);			
-		}
-		return str;
+    public static String reverseString(String s) {
+        String str = "";
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            str += s.charAt((length - 1) - i);
+        }
+        return str;
 
-	}
+    }
 
 }
