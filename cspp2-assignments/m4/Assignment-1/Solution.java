f94eef3359dfce236e4de252d8692633ee525a64
muralimandula <murali.mandula@msitprogram.net>
2018-08-31 15:42:23 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

---
+++ cspp2-assignments/m4/Assignment-1/Solution.java
@@ -0,0 +1,46 @@
+/**
+*20186065. 
+* To find the largest number in a given array.
+*/
+
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+
+public final class Solution {
+
+  /**
+   * Constructs the object.
+   */
+  private Solution() {
+    //unused
+  }
+
+
+  /**
+   * main function.
+   * @param      args  The arguments
+   */
+  public static void main(final String[] args) {
+    Scanner readInput = new Scanner(System.in);
+    int n = readInput.nextInt();
+
+    int[] arr = new int[n];
+
+    for (int i = 0; i < n; i++) {
+      arr[i] = readInput.nextInt();  // inserting into array
+    }
+
+    int max = arr[0];
+
+    for (int i = 1; i < n; i++) {
+      if (arr[i] > max) {
+        max = arr[i];
+      }
+    }
+
+    System.out.println(max);
+  }
+}
+
