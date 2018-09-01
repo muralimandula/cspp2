38e54ddf3e97bb236dd371ec03f1fcbc44e8263b
muralimandula <murali.mandula@msitprogram.net>
2018-09-01 10:29:34 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- cspp2-assignments/m6/Assignment-1/Solution.java
+++ cspp2-assignments/m6/Assignment-1/Solution.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 /**
- * Write a java program to find the odd composite numbers
+ * Write a java program to find the odd composite numbers.
  * between 2 and n where n is given as an input.
  *
- * @author : 
+ * @author :Muralidhar, 20186065.
  */
 public final class Solution {
     /**
@@ -12,33 +12,44 @@ public final class Solution {
     private Solution() {
         //not used
     }
+
+
+    /**
+     * Is prime checking.
+     * @param      n
+     * @return
+     */
+    
+    public static int isprime(int n) {
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+              count += 1;  
+            } 
+              }
+        if (count == 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
     /**
      * Prints the odd composite numbers between 2 and n.
      *
      * @param      n     n value
      */
-    static void oddComposites(final int n) {
-	// write your code here
+    public static void oddComposites(final int n) {
+    // write your code here
 
-    	for (int i=3; i < n; i++) {
+        for (int i = 3; i < n; i++) {
             int iprime = isprime(i);
-            if((i%2 != 0) && (iprime == 0)) System.out.println(i);
-		}
-    }
+            if ((i % 2 != 0) && (iprime == 0)) {
+                System.out.println(i);
+            }
+        }
+    }    
 
-    /**
-     * Is prime checking.
-     * @param      n 
-     * @return     
-     */
-    public static int isprime(int n) {
-		int count = 0;
-        for (int i = 2; i < n; i++) {
-        	if (n%i == 0) count += 1;
-              }
-        if (count == 0) return 1;
-        else return 0;
-	}
     /**
     * main method as driver program.
     * @param args is the parameter for this method
@@ -49,4 +60,3 @@ public final class Solution {
         oddComposites(n);
     }
 }
-
