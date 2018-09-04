875b9ab6993e718adba0e1af253ff6c5d8f8d05d
muralimandula <murali.mandula@msitprogram.net>
2018-09-04 13:59:24 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

---
+++ cspp2-assignments/m7/Assignment-1/Solution.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+/**
+ * another class for input validation.
+ */
+final class InputValidator {
+    
+
+    /**
+     * { var_description }.
+     */
+    private String input;
+    
+    /**
+     * Constructs the object.
+     *
+     * @param      inp  The input
+     */
+    InputValidator(final String inp) {
+        this.input = inp;
+    }
+    
+
+    /**
+     * { function_description }.
+     * makes use of this."created_object" with in InputValidator class.  
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public boolean validateData() {
+        final int val = 6;
+        if (input.length() >= val) {
+            return true;
+        }
+        return false;
+    }
+}
+
+
+/**
+ * Class for solution, Main.
+ */
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //unused dummy constructor.
+    }
+
+
+    /**
+     * { item_description }.
+     * @param args of type string
+     */
+    public static void main(final String[] args) {
+        Scanner readInput = new Scanner(System.in);
+        String input = readInput.nextLine();
+        InputValidator obj = new InputValidator(input); //Object created in InputValidator class.
+                                                    // Object "object" with attribute "input"
+        System.out.println(obj.validateData());
+    }
+}
