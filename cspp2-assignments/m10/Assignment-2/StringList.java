ad1f18419b4203dd955a90b3dbf4a64f369d1602
muralimandula <murali.mandula@msitprogram.net>
2018-09-06 19:31:22 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m10/Assignment-2/StringList.java
+++ cspp2-assignments/m10/Assignment-2/StringList.java
@@ -94,7 +94,8 @@ public class StringList implements StringListInterface {
         // What should be the default values?
         // In the case of the list, it should be empty but
         // it should be initialized with an array size like 10
-        list = new String[10];
+        final int listsize = 20;
+        list = new String[listsize];
 
         // Think about the initial value for size.
         // How many items do we have in the list when you create it?
