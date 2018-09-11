009c0a2364f169f662ce9b90ca2be7670128302e
muralimandula <murali.mandula@msitprogram.net>
2018-09-11 18:09:17 +0530

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

--- cspp2-assignments/m15/Assignment-2/Solution.java
+++ cspp2-assignments/m15/Assignment-2/Solution.java
@@ -7,28 +7,28 @@ import java.io.BufferedInputStream;
  * Exception for signaling invalid subset selection errors.
  */
 class InvalidSubsetSelectionException extends Exception {
-	/**
-	 * Constructs the object.
-	 *
-	 * @param      s     { parameter_description }
-	 */
-	InvalidSubsetSelectionException(final String s) {
-		super(s);
-	}
+    /**
+     * Constructs the object.
+     *
+     * @param      s     { parameter_description }
+     */
+    InvalidSubsetSelectionException(final String s) {
+        super(s);
+    }
 }
 
 /**
  * Exception for signaling set empty errors.
  */
 class SetEmptyException extends Exception {
-	/**
-	 * Constructs the object.
-	 *
-	 * @param      i     { parameter_description }
-	 */
-	SetEmptyException(final String i) {
-		super(i);
-	}
+    /**
+     * Constructs the object.
+     *
+     * @param      i     { parameter_description }
+     */
+    SetEmptyException(final String i) {
+        super(i);
+    }
 }
 
 
@@ -40,73 +40,73 @@ class SortedSet extends Set {
 
 
 
-	/**
-	 * Constructs the object.
-	 */
-	SortedSet() {
-		/**
-		 * Refers the the Parent class constructor.
-		 */
-		super();
-	}
-	/**
-	 * adds an element to the array.
-	 *
-	 * @param      item  The item
-	 */
-	@Override
-	public void add(final int item) {
-		if (this.size() == this.getarraylength() - 1) {
-			resize();
-		}
+    /**
+     * Constructs the object.
+     */
+    SortedSet() {
+        /**
+         * Refers the the Parent class constructor.
+         */
+        super();
+    }
+    /**
+     * adds an element to the array.
+     *
+     * @param      item  The item
+     */
+    @Override
+    public void add(final int item) {
+        if (this.size() == this.getarraylength() - 1) {
+            resize();
+        }
 
-		if (!contains(item)) {
-			this.insert(item);
-		}
+        if (!contains(item)) {
+            this.insert(item);
+        }
 
-	}
-	/**
-	 * insert an item in the array.
-	 *
-	 * @param      item  The item
-	 */
-	public void insert(final int item) {
-		int reqindex = 0;
-		for (int i = this.size() - 1; i >= 0; i--) {
-			if (this.get(i) < item) {
-				reqindex = i + 1;
-				break;
-			}
-		}
+    }
+    /**
+     * insert an item in the array.
+     *
+     * @param      item  The item
+     */
+    public void insert(final int item) {
+        int reqindex = 0;
+        for (int i = this.size() - 1; i >= 0; i--) {
+            if (this.get(i) < item) {
+                reqindex = i + 1;
+                break;
+            }
+        }
 
-		/*int temp = (size() == 0) ? 0 : this.get(reqindex);*/
-		int temp = 0;
-		if (size() == 0) {
-			temp = 0;
-		} else {
-			temp = this.get(reqindex);
-		}
-		for (int i = reqindex; i < size() + 1; i++) {
-			int tmp2 = this.get(i + 1);
-			this.arraysetter(temp, i + 1);
-			temp = tmp2;
+        /*int temp = (size() == 0) ? 0 : this.get(reqindex);*/
+        int temp = 0;
+        if (size() == 0) {
+            temp = 0;
+        } else {
+            temp = this.get(reqindex);
+        }
+        for (int i = reqindex; i < size() + 1; i++) {
+            int tmp2 = this.get(i + 1);
+            this.arraysetter(temp, i + 1);
+            temp = tmp2;
 
-		}
+        }
 
-		this.arraysetter(item, reqindex);
-		this.sizeinc();
+        this.arraysetter(item, reqindex);
+        this.sizeinc();
 
-	}
-	/**
-	 * Adds all.
-	 *
-	 * @param      eachint   The eachint
-	 */
-	public void addAll(final int[] eachint) {
-		for (int i : eachint) {
-			this.add(i);
-		}
-	}
+    }
+    /**
+     * Adds all.
+     *
+     * @param      eachint   The eachint
+     */
+    public void addAll(final int[] eachint) {
+        for (int i : eachint) {
+            this.add(i);
+        }
+    }
 
     /**
      * { function_description }.
@@ -118,32 +118,32 @@ class SortedSet extends Set {
      *
      * @throws     InvalidSubsetSelectionException  { exception_description }
      */
-	public int[] subSet(final int start, final int end) throws
-		InvalidSubsetSelectionException {
-		if (start > end) {
-			throw new InvalidSubsetSelectionException("");
+    public int[] subSet(final int start, final int end) throws
+        InvalidSubsetSelectionException {
+        if (start > end) {
+            throw new InvalidSubsetSelectionException("");
 
-		} else {
-			int i = 0;
-			int j = 0;
-			for (; i < size(); i++) {
-				if (get(i) >= start) {
-					break;
-				}
-			}
-			for (j = i; j < size(); j++) {
-				if (get(j) >= end) {
-					break;
-				}
-			}
-			int[] result = new int[j - i];
-			for (int k = 0; i < j; i++, k++) {
-				result[k] = get(i);
-			}
+        } else {
+            int i = 0;
+            int j = 0;
+            for (; i < size(); i++) {
+                if (get(i) >= start) {
+                    break;
+                }
+            }
+            for (j = i; j < size(); j++) {
+                if (get(j) >= end) {
+                    break;
+                }
+            }
+            int[] result = new int[j - i];
+            for (int k = 0; i < j; i++, k++) {
+                result[k] = get(i);
+            }
 
-			return result;
-		}
-	}
+            return result;
+        }
+    }
 
 
     /**
@@ -155,19 +155,19 @@ class SortedSet extends Set {
      *
      * @throws     SetEmptyException  { exception_description }
      */
-	public int[] headSet(final int element)  throws SetEmptyException {
-		if (element <= get(0)) {
-			throw new SetEmptyException("");
-		} else {
-			int i = 0;
-			for (; i < size(); i++) {
-				if (get(i) >= element) {
-					break;
-				}
-			}
-			return Arrays.copyOf(getArray(), i);
-		}
-	}
+    public int[] headSet(final int element)  throws SetEmptyException {
+        if (element <= get(0)) {
+            throw new SetEmptyException("");
+        } else {
+            int i = 0;
+            for (; i < size(); i++) {
+                if (get(i) >= element) {
+                    break;
+                }
+            }
+            return Arrays.copyOf(getArray(), i);
+        }
+    }
 
     /**
      * { function_description }.
@@ -176,13 +176,13 @@ class SortedSet extends Set {
      *
      * @throws     SetEmptyException  { exception_description }
      */
-	public int last() throws SetEmptyException {
-		if (this.size() == 0) {
-			throw new SetEmptyException("");
-		} else {
-			return get(this.size() - 1);
-		}
-	}
+    public int last() throws SetEmptyException {
+        if (this.size() == 0) {
+            throw new SetEmptyException("");
+        } else {
+            return get(this.size() - 1);
+        }
+    }
 }
 
 
@@ -190,113 +190,113 @@ class SortedSet extends Set {
  * Solution class for code-eval.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * helper function to convert string input to int array.
-	 *
-	 * @param      s     { string input from test case file }
-	 *
-	 * @return     { int array from the given string }
-	 */
-	public static int[] intArray(final String s) {
-		String input = s;
-		if (input.equals("[]")) {
-			return new int[0];
-		}
-		if (s.contains("[")) {
-			input = s.substring(1, s.length() - 1);
-		}
-		return Arrays.stream(input.split(","))
-		       .mapToInt(Integer::parseInt)
-		       .toArray();
-	}
-	/**
-	 * main function to execute test cases.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		// instantiate this set
-		SortedSet s = new SortedSet();
-		// code to read the test cases input file
-		Scanner stdin = new Scanner(new BufferedInputStream(System.in));
-		// check if there is one more line to process
-		while (stdin.hasNext()) {
-			// read the line
-			String line = stdin.nextLine();
-			// split the line using space
-			String[] tokens = line.split(" ");
-			// based on the list operation invoke the corresponding method
-			switch (tokens[0]) {
-			case "print":
-				System.out.println(s);
-				break;
-			case "add":
-				int[] intArray = intArray(tokens[1]);
-				s.add(intArray[0]);
-				break;
-			case "addAll":
-				intArray = intArray(tokens[1]);
-				s.addAll(intArray);
-				break;
-			case "subSet":
-				intArray = intArray(tokens[1]);
-				try {
-					int[] subsetarray = s.subSet(intArray[0], intArray[1]);
-					if (subsetarray != null) {
-						System.out.println(Arrays.toString(subsetarray).
-						                   replace("[", "{").replace("]", "}"));
-					}
-				} catch (InvalidSubsetSelectionException e) {
-					System.out.println("Invalid Arguments to Subset Exception");
-				}
+    }
+    /**
+     * helper function to convert string input to int array.
+     *
+     * @param      s     { string input from test case file }
+     *
+     * @return     { int array from the given string }
+     */
+    public static int[] intArray(final String s) {
+        String input = s;
+        if (input.equals("[]")) {
+            return new int[0];
+        }
+        if (s.contains("[")) {
+            input = s.substring(1, s.length() - 1);
+        }
+        return Arrays.stream(input.split(","))
+               .mapToInt(Integer::parseInt)
+               .toArray();
+    }
+    /**
+     * main function to execute test cases.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        // instantiate this set
+        SortedSet s = new SortedSet();
+        // code to read the test cases input file
+        Scanner stdin = new Scanner(new BufferedInputStream(System.in));
+        // check if there is one more line to process
+        while (stdin.hasNext()) {
+            // read the line
+            String line = stdin.nextLine();
+            // split the line using space
+            String[] tokens = line.split(" ");
+            // based on the list operation invoke the corresponding method
+            switch (tokens[0]) {
+            case "print":
+                System.out.println(s);
+                break;
+            case "add":
+                int[] intArray = intArray(tokens[1]);
+                s.add(intArray[0]);
+                break;
+            case "addAll":
+                intArray = intArray(tokens[1]);
+                s.addAll(intArray);
+                break;
+            case "subSet":
+                intArray = intArray(tokens[1]);
+                try {
+                    int[] subsetarray = s.subSet(intArray[0], intArray[1]);
+                    if (subsetarray != null) {
+                        System.out.println(Arrays.toString(subsetarray).
+                                    replace("[", "{").replace("]", "}"));
+                    }
+                } catch (InvalidSubsetSelectionException e) {
+                System.out.println("Invalid Arguments to Subset Exception");
+                }
 
-				break;
-			case "headSet":
-				intArray = intArray(tokens[1]);
-				try {
-					System.out.println(Arrays.toString(
-					                       s.headSet(intArray[0])).replace(
-					                       "[", "{").replace("]", "}"));
-				} catch (SetEmptyException e) {
-					System.out.println("Set Empty Exception");
-				}
+                break;
+            case "headSet":
+                intArray = intArray(tokens[1]);
+                try {
+                    System.out.println(Arrays.toString(
+                                           s.headSet(intArray[0])).replace(
+                                           "[", "{").replace("]", "}"));
+                } catch (SetEmptyException e) {
+                    System.out.println("Set Empty Exception");
+                }
 
-				break;
-			case "last":
-				try {
-					System.out.println(s.last());
-				} catch (SetEmptyException eset) {
-					System.out.println("Set Empty Exception");
-				}
-				break;
+                break;
+            case "last":
+                try {
+                    System.out.println(s.last());
+                } catch (SetEmptyException eset) {
+                    System.out.println("Set Empty Exception");
+                }
+                break;
 
-			case "intersection":
-				s = new SortedSet();
-				Set t = new Set();
-				intArray = intArray(tokens[1]);
-				s.add(intArray);
-				intArray = intArray(tokens[2]);
-				t.add(intArray);
-				System.out.println(s.intersection(t));
-				break;
+            case "intersection":
+                s = new SortedSet();
+                Set t = new Set();
+                intArray = intArray(tokens[1]);
+                s.add(intArray);
+                intArray = intArray(tokens[2]);
+                t.add(intArray);
+                System.out.println(s.intersection(t));
+                break;
 
-			case "retainAll":
-				s = new SortedSet();
-				intArray = intArray(tokens[1]);
-				s.add(intArray);
-				intArray = intArray(tokens[2]);
-				System.out.println(s.retainAll(intArray));
-				break;
+            case "retainAll":
+                s = new SortedSet();
+                intArray = intArray(tokens[1]);
+                s.add(intArray);
+                intArray = intArray(tokens[2]);
+                System.out.println(s.retainAll(intArray));
+                break;
 
-			default:
-				break;
-			}
-		}
-	}
+            default:
+                break;
+            }
+        }
+    }
 }
