1b07a7dfac2c607c68e944f0a3c21a1e0791b9a2
muralimandula <murali.mandula@msitprogram.net>
2018-09-12 14:24:27 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ cspp2-assignments/m16/bookyourshow/Solution.java
@@ -0,0 +1,107 @@
+import java.io.BufferedInputStream;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Solution{
+
+
+	public String[] byshow;
+
+	public int size;
+	// public String[] showtime;
+	// public String[] Seats;
+	// public String[] user;
+	// public int[] mobilenum;
+	// public String[] booking;
+	//
+	public Solution()
+	{
+	byshow = new String[100];
+	size = 0;
+	}
+
+
+	public void add(final String addmovie) {
+		byshow[size] = addmovie;
+		size++;
+	}
+
+
+	public String getmovie(final String moviename, final String showtime) {
+		for(int i = 0; i < size; i++) {
+			// System.out.println(byshow[i]);
+			// System.out.println(moviename);
+			if(byshow[i].contains(moviename) && byshow[i].contains(showtime)) {
+				return byshow[i];
+			}
+		}
+		return "No show";
+	}
+
+
+    public String printbooking(final String moviename, final String showbooked, final String usercontact ) {
+       		for(int i = 0; i < byshow.length; i++) {
+			if(byshow[i].contains(moviename) && byshow[i].contains(usercontact)) {
+				return usercontact + " " + moviename + " " + showbooked;
+			}
+		}
+		return "No Booking";
+    }
+
+	public static void main(String[] args) {
+	   	Solution addmovieobj = new Solution();
+	    // Solution bookobj = new Solution();
+		Scanner input = new Scanner(new BufferedInputStream(System.in));
+		while (input.hasNext()){
+
+
+			String readinput = input.nextLine();
+			String[] bysinput = readinput.split(","); //to take case add/book
+			String[] bysmovie = bysinput[0].split(" ");
+			String moviename = bysmovie[1];
+			String[] bysinput2 = readinput.split(" "); // to operate booking and adding
+
+			switch (bysmovie[0]) {
+
+	     		case "add":
+					String bysseats = bysinput2[4].substring(6);
+					String movie;
+					movie = moviename+"-"+bysinput[1]+"-"+bysseats;
+					// System.out.println(movie);
+					addmovieobj.add(movie);
+					break;
+
+				case "book":
+				    String userbooking;
+				    String userseats = bysinput2[4].substring(24); // works for case1, because username is 6 chars
+				    userbooking = moviename+"-"+bysinput[1]+"-"+bysinput[2]+"-"+bysinput[3]+"-"+userseats;
+                    // moviename - TimeStamp - username - usercontact
+
+                     // bookobj.add(userbooking);
+                    System.out.println(userbooking);
+                    addmovieobj.add(userbooking);
+                    break;
+
+                case "get":
+                	String showtime = bysinput[1];
+                    System.out.println(addmovieobj.getmovie(moviename, showtime));
+                    break;
+
+                case "print":
+                    String showbooked = bysinput[1];
+                    String usercontact = bysinput[2];
+                    // System.out.println(moviename + "-" + showtime + "-" + usercontact);
+                    System.out.println(addmovieobj.printbooking(moviename, showbooked, usercontact));
+                    break;
+
+                // case "show movies":
+                //     System.out.println(addmovieobj.showmovies());
+                //     break;
+
+                default:
+                	break;
+			}
+		}
+	}
+
+}
